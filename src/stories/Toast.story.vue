e6b7a0dece6882a2a87fbe5fb1d7a60e29afecc2
Anwarul Islam <anwaarulislaam@gmail.com>
2024-03-04 11:20:18 +0600

    feat: vue soner added

---
+++ src/stories/Toast.story.vue
@@ -0,0 +1,14 @@
+<template>
+  <Story title="Toast">
+    <Toaster />
+    <button @click="openToast">Basic Toast</button>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import { Toaster, toast } from "vue-sonner"
+
+const openToast = () => {
+  toast("Basic toast")
+}
+</script>
