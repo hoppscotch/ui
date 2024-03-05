dacf0c1e8b9a2dae6baf67712854cc587aeaa31b
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-03-05 17:47:45 +0530

    feat: sort, pagination, spinner and checkbox added to table component (#5)
    Co-authored-by: jamesgeorge007 <jamesgeorge998001@gmail.com>

--- src/stories/Table.story.vue
+++ src/stories/Table.story.vue
@@ -1,39 +1,85 @@
 <template>
   <Story title="Table">
     <Variant title="General">
-      <HoppSmartTable :list="list" :headings="headings" />
+      <HoppSmartTable
+        :headings="headings"
+        :loading="loading"
+        :list="finalList"
+        :selected-rows="selectedRows"
+        :pagination="{ totalPages: 2 }"
+        @page-number="handlePageChange"
+      >
+      </HoppSmartTable>
     </Variant>
+
+    <!-- Custom implementation of the Table -->
     <Variant title="Custom">
-      <HoppSmartTable>
+      <HoppSmartTable :loading="loading" :list="finalList">
         <template #head>
-          <tr
-            class="text-sm text-left border-b border-dividerDark bg-primaryLight text-secondary"
+          <th
+            v-for="heading in headings"
+            :key="heading.key"
+            scope="col"
+            class="px-6 py-3"
           >
-            <th
-              v-for="heading in headings"
-              :key="heading.key"
-              scope="col"
-              class="px-6 py-3"
-            >
-              {{ heading.label }}
-            </th>
-          </tr>
+            {{ heading.label }}
+          </th>
         </template>
 
-        <template #body>
-          <tr
-            v-for="item in list"
-            :key="item.id"
-            class="rounded-xl text-secondaryDark hover:cursor-pointer hover:bg-divider"
+        <template #loading-state>
+          <td :colspan="headings.length">
+            <div class="mx-auto my-3 h-5 w-5 text-center">
+              <HoppSmartSpinner />
+            </div>
+          </td>
+        </template>
+
+        <template #body="{ row }">
+          <td
+            v-for="cellHeading in headings"
+            :key="cellHeading.key"
+            class="max-w-[10rem] py-1 pl-6"
           >
-            <td
-              v-for="cellHeading in headings"
-              :key="cellHeading.key"
-              class="max-w-[10rem] py-1 pl-6"
-            >
-              {{ item[cellHeading.key] ?? "-" }}
-            </td>
-          </tr>
+            {{ row[cellHeading.key] ?? "-" }}
+          </td>
+        </template>
+
+        <template #empty-state>
+          <td :colspan="headings.length" class="py-3 text-center">
+            <p>No data available</p>
+          </td>
+        </template>
+      </HoppSmartTable>
+    </Variant>
+
+    <!-- Extending the Table functionality -->
+    <Variant title="Extension">
+      <HoppSmartTable
+        :headings="headings"
+        :loading="loading"
+        :list="extensionList"
+        :selected-rows="selectedRows"
+        :sort="{ key: 'name', direction: sortDirection }"
+        @page-number="handlePageChange"
+      >
+        <template #extension>
+          <div class="flex">
+            <div class="flex w-full items-center bg-primary">
+              <icon-lucide-search class="mx-3 text-xs" />
+              <HoppSmartInput
+                v-model="searchQuery"
+                styles="w-full bg-primary py-1"
+                input-styles="h-full border-none"
+                placeholder="Search.."
+              />
+            </div>
+            <HoppButtonPrimary
+              :icon="IconArrowUpDown"
+              label="Sort"
+              class="rounded-none"
+              @click="toggleSortDirection"
+            />
+          </div>
         </template>
       </HoppSmartTable>
     </Variant>
@@ -41,8 +87,19 @@
 </template>
 
 <script setup lang="ts">
-import { HoppSmartTable } from "../components/smart"
-import { CellHeading } from "~/components/smart/Table.vue"
+import { computed, onMounted, ref, Ref } from "vue"
+
+import { CellHeading, Direction } from "~/components/smart/Table.vue"
+import IconArrowUpDown from "~icons/lucide/arrow-up-down"
+import { HoppButtonPrimary, HoppSmartInput } from ".."
+import { HoppSmartSpinner, HoppSmartTable } from "../components/smart"
+
+type List = {
+  id: string
+  name: string
+  members: number
+  role: string
+}
 
 // Table Headings
 const headings: CellHeading[] = [
@@ -52,18 +109,76 @@ const headings: CellHeading[] = [
   { key: "role", label: "Role" },
 ]
 
-const list: Record<string, string | number>[] = [
-  {
-    id: "123455",
-    name: "Joel",
-    members: 10,
-    role: "Frontend Engineer",
-  },
+const loading = ref(false)
+
+const finalList = ref<List[]>([])
+const selectedRows = ref<List[]>([])
+
+const primaryList: List[] = [
   {
     id: "123456",
-    name: "Anwar",
+    name: "Walter",
     members: 12,
-    role: "Frontend Engineer",
+    role: "Chemical Engineer",
+  },
+  {
+    id: "123455",
+    name: "Jesse",
+    members: 10,
+    role: "Lab Assistant",
   },
 ]
+
+const secondaryList: List[] = [
+  {
+    id: "123457",
+    name: "Gus",
+    members: 20,
+    role: "CEO",
+  },
+  {
+    id: "123458",
+    name: "Mike",
+    members: 15,
+    role: "Security",
+  },
+]
+
+onMounted(async () => {
+  loading.value = true
+
+  // Simulate network call
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  finalList.value = primaryList
+  loading.value = false
+})
+
+const handlePageChange = (pageNumber: number) => {
+  if (pageNumber === 1) {
+    finalList.value = primaryList
+  } else {
+    finalList.value = secondaryList
+  }
+}
+
+const sortDirection: Ref<Direction> = ref("ascending")
+
+const toggleSortDirection = () => {
+  sortDirection.value =
+    sortDirection.value === "ascending" ? "descending" : "ascending"
+}
+
+const searchQuery = ref("")
+
+const extensionList = computed(() => {
+  return primaryList.filter((item) => {
+    return Object.values(item).some((value) =>
+      value
+        .toString()
+        .toLowerCase()
+        .includes((searchQuery.value as string).toLowerCase()),
+    )
+  })
+})
 </script>
