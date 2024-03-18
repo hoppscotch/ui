13cac11c12504fa0b9d39ea6f41db6c362aa6b15
Andrew Bastin <andrewbastin.k@gmail.com>
2024-03-18 21:38:59 +0530

    chore: initial impl

---
+++ src/GlobalWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <DialogHost />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { DialogHost } from "vue-promise-modals"
+</script>
+
+<style>
+.htw-sandbox-hidden {
+  display: block;
+}
+</style>
---
+++ src/stories/NewModal.story.vue
@@ -0,0 +1,18 @@
+<template>
+  <Story title="New Modal">
+    <button @click="openTestModal">Open Modal</button>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import { useModals } from "vue-promise-modals"
+import { HoppModal } from "../components/modal"
+
+const { openModal } = useModals()
+
+function openTestModal() {
+  openModal(HoppModal, {
+    openCount: 0
+  })
+}
+</script>
