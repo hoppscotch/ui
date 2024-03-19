9bca7b662e5f8a00cb5590b73aa39d3fff192a56
Anwarul Islam <anwaarulislaam@gmail.com>
2024-03-19 17:55:13 +0600

    feat: simple and input modal story added

--- src/components/modal/index.vue
+++ src/components/modal/index.vue
@@ -3,7 +3,17 @@
     <div class="dialog-header">
       <h2>Simple Dialog</h2>
       <button class="dialog-close" @click="emit('modal-resolve', {})">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"/></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"
+          />
+        </svg>
       </button>
     </div>
     <div>
@@ -29,3 +39,10 @@ onMounted(() => {
   dialog.value?.showModal()
 })
 </script>
+
+<style lang="scss" scoped>
+dialog {
+  padding: 0 !important;
+  border: 0 !important;
+}
+</style>
--- src/stories/NewModal.story.vue
+++ src/stories/NewModal.story.vue
@@ -1,18 +1,42 @@
 <template>
-  <Story title="New Modal">
-    <button @click="openTestModal">Open Modal</button>
+  <Story title="New Modal (HoppModal)">
+    <Variant title="Simple Dialog with Close Button">
+      <button @click="openTestModal">Open Simple Dialog</button>
+    </Variant>
+    <Variant title="Dialog with Input">
+      <p>
+        <span>
+          Resolved Value: <strong>{{ text }}</strong>
+        </span>
+      </p>
+
+      <button @click="openInputDialog">Open Dialog with Input</button>
+    </Variant>
   </Story>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue"
 import { useModals } from "vue-promise-modals"
-import { HoppModal } from "../components/modal"
+import { HoppModal, InputDialog } from "../components/modal"
 
 const { openModal } = useModals()
 
 function openTestModal() {
   openModal(HoppModal, {
-    openCount: 0
+    openCount: 0,
   })
+}
+
+const text = ref("")
+
+async function openInputDialog() {
+  try {
+    const result = await openModal(InputDialog)
+    text.value = result.text
+  } catch (e) {
+    // The error value will be the same value as emitted through the `modal-reject` event
+    console.log("Modal Rejected:", e)
+  }
 }
 </script>
