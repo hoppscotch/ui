2bdf26afe4894f62f935d172356374e1e7e4157c
Anwarul Islam <anwaarulislaam@gmail.com>
2024-04-08 11:34:21 +0600

    feat: legacy toast support

--- src/helpers/toast.ts
+++ src/helpers/toast.ts
@@ -1,13 +1,30 @@
 import { toast as soner } from "vue-sonner"
 
-type ToastOptions = {
+export type ToastOptions = {
   type: "success" | "error" | "warning" | "info"
   duration: number
 }
 
 export const toast = (message: string, option?: ToastOptions) => {
-  console.log("toast")
   soner(message, {
+    ...option,
+  })
+}
+
+toast.success = (message: string, option?: ToastOptions) => {
+  soner.success(message, {
+    ...option,
+  })
+}
+
+toast.error = (message: string, option?: ToastOptions) => {
+  soner.error(message, {
+    ...option,
+  })
+}
+
+toast.warning = (message: string, option?: ToastOptions) => {
+  soner.warning(message, {
     ...option,
   })
 }
--- src/stories/Toast.story.vue
+++ src/stories/Toast.story.vue
@@ -1,7 +1,12 @@
 <template>
   <Story title="Toast">
     <Toaster />
-    <button @click="openToast">Basic Toast</button>
+    <Variant title="Legacy Toast">
+      <button @click="launchLegacyToast">Simple Legacy toast</button>
+    </Variant>
+    <Variant title="New Toast">
+      <button @click="openToast">Basic Toast</button>
+    </Variant>
   </Story>
 </template>
 
@@ -11,5 +16,9 @@ import { toast } from "./../helpers/toast"
 
 const openToast = () => {
   toast("Basic toast")
+}
+
+const launchLegacyToast = () => {
+  toast.success("Simple Legacy toast")
 }
 </script>
