dabdda781ce8cb8a2bb6d994519417d8fe4dfeca
Anwarul Islam <anwaarulislaam@gmail.com>
2024-04-18 20:36:39 +0600

    feat: legacy modal story

--- src/stories/Modal.story.vue
+++ src/stories/Modal.story.vue
@@ -1,10 +1,12 @@
 <template>
   <Story title="Modal (Deprecated)">
+    <button @click="showModal()">Show Modal</button>
+
     <HoppSmartModal
       :show="show"
       :title="'Modal Title'"
       @hide-modal="show = false"
-      @resolve="resolveConfirmModal"
+      @resolve="resolveConfirmModal($event)"
     />
   </Story>
 </template>
@@ -13,10 +15,26 @@
 import { HoppSmartModal } from "../components/smart"
 import { ref } from "vue"
 
-const show = ref(true)
+const show = ref(false)
 
 const resolveConfirmModal = (resolve: string | null) => {
   alert("resolved: " + resolve)
   show.value = false
 }
+
+const showModal = () => {
+  show.value = true
+}
 </script>
+
+<style scoped>
+button {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f5f5f5;
+  cursor: pointer;
+  color: #333;
+}
+</style>
