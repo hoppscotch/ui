55fdc20e3ec4ec33f21e4eb7d7428a148237c265
Arpit Singh <arpitsingh@Mac.mshome.net>
2024-07-27 18:31:54 +0530

    made the checkbox on by default using the vue code and remove ref which was causing the error

--- src/components/smart/Checkbox.vue
+++ src/components/smart/Checkbox.vue
@@ -39,7 +39,7 @@ const checkboxID = `checkbox-${checkboxIDCounter++}`
 defineProps({
   on: {
     type: Boolean,
-    default: false,
+    default: true, // Set default to true
   },
   name: {
     type: String,
