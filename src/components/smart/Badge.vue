5b20d24c436c8c61b0023c8fe14e8a94afc73d0a
Arpit Singh <arpitsingh@Mac.mshome.net>
2024-07-27 18:23:21 +0530

    Setup a basic badge component

---
+++ src/components/smart/Badge.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>hi</div>
+  </template>
+ 
+  <script setup lang="ts">
+  
+  </script>
+ 
+  <style lang="scss" scoped>
+
+  </style>
---
+++ src/stories/Badge.story.vue
@@ -0,0 +1,12 @@
+<template>
+    <Story title="Badge">
+      <Variant title="Single">
+        <HoppSmartBadge />
+    </Variant>
+    </Story>
+  </template>
+  
+  <script setup lang="ts">
+  import { HoppSmartBadge } from "../components/smart"
+  </script>
+  
